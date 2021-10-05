e9b508a96088947dd6d4e59b80246a7d585492b4
CaffeineProgrammer <chris.hu@atos.net>
2021-10-05 11:45:38 +0200

    Added competence to the routing

--- competence-dev-web-app/src/app/app-routing.module.ts
+++ competence-dev-web-app/src/app/app-routing.module.ts
@@ -1,26 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CompetenceComponent } from './components/competence/competence.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo:'home', pathMatch: 'full'
+    redirectTo: 'home',
+    pathMatch: 'full',
   },
   {
     path: 'home',
     component: HomeComponent,
   },
   {
+    path: 'competence',
+    component: CompetenceComponent,
+  },
+  {
     path: 'dashboard',
     component: DashboardComponent,
   },
-  
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- competence-dev-web-app/src/app/app.component.ts
+++ competence-dev-web-app/src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  templateUrl: './components/competence/competence.component.html',
-  // styleUrls: ['./app.component.css']
-  styleUrls: ['./components/competence/competence.component.css'],
+  templateUrl: './app.component.html',
+  // templateUrl: './components/competence/competence.component.html',
+  styleUrls: ['./app.component.scss']
+  // styleUrls: ['./components/competence/competence.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'AMS Competence Web App';
