253019e5511c78688db945daf0ca45630aa547db
CaffeineProgrammer <chris.hu@atos.net>
2021-10-13 14:22:20 +0200

    Changed ngFor

--- competence-dev-web-app/src/app/unit-card/unit-card.component.ts
+++ competence-dev-web-app/src/app/unit-card/unit-card.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-unit-card',
   templateUrl: './unit-card.component.html',
-  styleUrls: ['./unit-card.component.css']
+  styleUrls: ['./unit-card.component.css'],
 })
 export class UnitCardComponent implements OnInit {
+  units: Unit = [
+    { title: 'Java', content: 'test', colorId: 1 },
+    { title: '.NET', content: 'test', colorId: 1 },
+    { title: 'Testing', content: 'test', colorId: 2 },
+    { title: 'Low Coding', content: 'test', colorId: 2 },
+    { title: 'Business Analyse', content: 'test', colorId: 3 },
+    { title: 'Oracle', content: 'test', colorId: 3 },
+    { title: 'Cloud', content: 'test', colorId: 4 },
+    { title: 'Agile', content: 'test', colorId: 4 },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
+
+type Unit = Array<{ title: string; content: string; colorId: number }>;
