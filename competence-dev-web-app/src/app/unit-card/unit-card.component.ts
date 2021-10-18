f4b123e8d8000cdcdce7913f028059dd00a721f1
CaffeineProgrammer <chris.hu@atos.net>
2021-10-18 23:17:59 +0200

    Added favorite functionality

--- competence-dev-web-app/src/app/unit-card/unit-card.component.ts
+++ competence-dev-web-app/src/app/unit-card/unit-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-unit-card',
@@ -6,56 +6,89 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./unit-card.component.scss'],
 })
 export class UnitCardComponent implements OnInit {
+  // @Input() selected: boolean;
+  // @Output() selectedChange = new EventEmitter<boolean>();
+
   units: Unit = [
     {
       title: 'Java',
       content: 'test',
       class: 'unit-card-container__blue',
+      favorite: false,
     },
     {
       title: '.NET',
       content: 'test',
       class: 'unit-card-container__blue',
+      favorite: false,
     },
     {
       title: 'Testing',
       content: 'test',
       class: 'unit-card-container__green',
+      favorite: false,
     },
     {
       title: 'Low Coding',
       content: 'test',
       class: 'unit-card-container__green',
+      favorite: false,
     },
     {
       title: 'Business Analyse',
       content: 'test',
       class: 'unit-card-container__purple',
+      favorite: false,
     },
     {
       title: 'Oracle',
       content: 'test',
       class: 'unit-card-container__purple',
+      favorite: false,
     },
     {
       title: 'Cloud',
       content: 'test',
       class: 'unit-card-container__red',
+      favorite: false,
     },
     {
       title: 'Agile',
       content: 'test',
       class: 'unit-card-container__red',
+      favorite: false,
     },
   ];
 
-  constructor() {}
+  constructor() {
+    // this.selected = false;
+  }
 
   ngOnInit(): void {}
+
+  public toggleFavoriteButton(name: string) {
+    this.units.find((e, i) => {
+      if (e.title === name) {
+        if (this.units[i].favorite === false) {
+          this.units[i].favorite = true;
+        } else {
+          this.units[i].favorite = false;
+        }
+      }
+    });
+
+    console.log(this.units.find((e) => e.title === name));
+
+    // console.log(name);
+
+    //   this.selected = !this.selected;
+    //   this.selectedChange.emit(this.selected);
+  }
 }
 
 type Unit = Array<{
   title: string;
   content: string;
   class: string;
+  favorite: boolean;
 }>;
