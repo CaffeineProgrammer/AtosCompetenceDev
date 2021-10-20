0536a428e6bdfc6b00439fb175e56279cc1e2e05
CaffeineProgrammer <chris.hu@atos.net>
2021-10-20 11:11:30 +0200

    Moved two components into competence folder

--- competence-dev-web-app/src/app/app-routing.module.ts
+++ competence-dev-web-app/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { CompetencePageComponent } from './competence-page/competence-page.component';
+import { CompetencePageComponent } from './competences/competence-page/competence-page.component';
 import { InfopageComponent } from './infopage/infopage.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
