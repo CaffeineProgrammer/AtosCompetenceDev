7775ff563a51b4086f2b3472a325b02683b45bf7
CaffeineProgrammer <chris.hu@atos.net>
2021-10-22 11:37:19 +0200

    Added extra mock data

--- competence-dev-web-app/src/app/mock-competences.ts
+++ competence-dev-web-app/src/app/mock-competences.ts
@@ -6,13 +6,20 @@ export const COMPETENCES = [
         competenceName: 'Cloud computing',
         presenter: 'Frits',
         content: 'test',
+        class: 'competence-card-container--blue',
       },
       {
         competenceName: 'Cloud startup',
         presenter: 'Frits',
         content: 'test',
+        class: 'competence-card-container--blue',
       },
-      { competenceName: 'Cloud', presenter: 'Mark', content: 'test' },
+      {
+        competenceName: 'Cloud',
+        presenter: 'Mark',
+        content: 'test',
+        class: 'competence-card-container--blue',
+      },
     ],
   },
   {
@@ -22,11 +29,115 @@ export const COMPETENCES = [
         competenceName: 'Mock name',
         presenter: 'Mock presenter',
         content: 'Mock content',
+        class: 'competence-card-container--blue',
       },
       {
         competenceName: 'Mock name',
         presenter: 'Mock presenter',
         content: 'Mock content',
+        class: 'competence-card-container--blue',
+      },
+    ],
+  },
+  {
+    unitName: 'Testing',
+    competence: [
+      {
+        competenceName: 'Mock name',
+        presenter: 'Mock presenter',
+        content: 'Mock content',
+        class: 'competence-card-container--green',
+      },
+      {
+        competenceName: 'Mock name',
+        presenter: 'Mock presenter',
+        content: 'Mock content',
+        class: 'competence-card-container--green',
+      },
+    ],
+  },
+  {
+    unitName: 'Low Coding',
+    competence: [
+      {
+        competenceName: 'Mock name',
+        presenter: 'Mock presenter',
+        content: 'Mock content',
+        class: 'competence-card-container--green',
+      },
+      {
+        competenceName: 'Mock name',
+        presenter: 'Mock presenter',
+        content: 'Mock content',
+        class: 'competence-card-container--green',
+      },
+    ],
+  },
+  {
+    unitName: 'Business Analyse',
+    competence: [
+      {
+        competenceName: 'Mock name',
+        presenter: 'Mock presenter',
+        content: 'Mock content',
+        class: 'competence-card-container--purple',
+      },
+      {
+        competenceName: 'Mock name',
+        presenter: 'Mock presenter',
+        content: 'Mock content',
+        class: 'competence-card-container--purple',
+      },
+    ],
+  },
+  {
+    unitName: 'Oracle',
+    competence: [
+      {
+        competenceName: 'Mock name',
+        presenter: 'Mock presenter',
+        content: 'Mock content',
+        class: 'competence-card-container--purple',
+      },
+      {
+        competenceName: 'Mock name',
+        presenter: 'Mock presenter',
+        content: 'Mock content',
+        class: 'competence-card-container--purple',
+      },
+    ],
+  },
+  {
+    unitName: 'Cloud',
+    competence: [
+      {
+        competenceName: 'Mock name',
+        presenter: 'Mock presenter',
+        content: 'Mock content',
+        class: 'competence-card-container--red',
+      },
+      {
+        competenceName: 'Mock name',
+        presenter: 'Mock presenter',
+        content: 'Mock content',
+        class: 'competence-card-container--red',
+      },
+    ],
+  },
+  {
+    unitName: 'Agile',
+    competence: [
+      {
+        competenceName: 'Mock name',
+        presenter: 'Mock presenter',
+        content: 'Mock content',
+        class: 'competence-card-container--red',
+      },
+      {
+        competenceName: 'Mock name',
+        presenter: 'Mock presenter',
+        content: 'Mock content',
+        class: 'competence-card-container--red',
       },
     ],
   },
