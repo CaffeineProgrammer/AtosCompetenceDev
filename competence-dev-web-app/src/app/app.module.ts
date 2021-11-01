6c4edcc91149e000240607c69f2cf2eacca7f2ee
dMarkb <mark.mulder@atos.net>
2021-11-01 16:26:58 +0100

    Revert "Revert "Calendar position + minor fixes""
    This reverts commit 672d3a98007ecb7ac71b7b7d9481c10a40f2693c.

--- competence-dev-web-app/src/app/app.module.ts
+++ competence-dev-web-app/src/app/app.module.ts
@@ -1,35 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
-
-import { AppComponent } from './app.component';
-
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UnitCardComponent } from './unit-card/unit-card.component';
-import { CompetencePageComponent } from './competence-page/competence-page.component';
-import { CompetenceCardComponent } from './competence-card/competence-card.component';
-import { DropDownComponent } from './drop-down/drop-down.component';
-import { InfopageComponent } from './infopage/infopage.component';
-import { InfoComponent } from './info/info.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatCardModule } from '@angular/material/card';
+import { HomeComponent } from './components/home/home.component';
+import { CompetenceComponent } from './components/competence/competence.component';
+import { CompetenceCardComponent } from './components/competence-card/competence-card.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatMenuModule } from '@angular/material/menu';
+import { DropDownComponent } from './components/drop-down/drop-down.component';
+import { InfopageComponent } from './components/infopage/infopage.component';
+import { InfoComponent } from './components/info/info.component';
 import { CalendarComponent } from './components/calendar/calendar.component'
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
@@ -40,12 +35,11 @@ import { MatNativeDateModule } from '@angular/material/core';
   declarations: [
     AppComponent,
     HomeComponent,
+    CompetenceComponent,
+    CompetenceCardComponent,
     HeaderComponent,
     SidenavComponent,
     DashboardComponent,
-    UnitCardComponent,
-    CompetencePageComponent,
-    CompetenceCardComponent,
     DropDownComponent,
     InfopageComponent,
     InfoComponent,
@@ -53,8 +47,8 @@ import { MatNativeDateModule } from '@angular/material/core';
   ],
   imports: [
     BrowserModule,
-    RouterModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
     MatTabsModule,
     MatFormFieldModule,
     MatSelectModule,
